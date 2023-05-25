6a3d9c21f402d95bc648a8240c0960fffb63fae7
virdir2 <rallavirdi@eaton.com>
2023-05-25 15:53:13 -0400

    Assemblers page ability to scan discrete numbers: needed for Milton

--- Utility.cs
+++ Utility.cs
@@ -1352,5 +1352,17 @@ namespace PRL123_Final
         }
 
 
+        // The following is used to determine if a string contains digits only
+        public static Boolean IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+            return true;
+        }
+
+
     }
 }
